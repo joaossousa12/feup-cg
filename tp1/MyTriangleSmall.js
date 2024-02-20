e9d6cd8079091e4695245ad794889b97b8a04fa7
joao <up202106996@up.pt>
2024-02-20 11:59:33 +0000

    Finished TP1

---
+++ tp1/MyTriangleSmall.js
@@ -0,0 +1,24 @@
+import { CGFobject } from "../lib/CGF.js";
+
+export class MyTriangleSmall extends CGFobject {
+    constructor(scene){
+        super(scene);
+
+        this.initBuffers();
+    }
+
+    initBuffers(){
+        this.vertices = [
+            -1, 0, 0,
+            1, 0, 0,
+            0, 1, 0
+        ];
+
+        this.indices = [
+            0, 1, 2
+        ];
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+}
